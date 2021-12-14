5e63b34e7e4bf366cff944b91a682d13e33d9787
makarushko <71987147+makarushko@users.noreply.github.com>
2021-12-14 05:14:46 +0300

    Update Command.java

--- wt3/wt3/src/main/java/by/bsuir/anna/wt/tasks/third/server/command/Command.java
+++ wt3/wt3/src/main/java/by/bsuir/anna/wt/tasks/third/server/command/Command.java
@@ -1,6 +1,6 @@
-package by.bsuir.dshparko.wt.tasks.third.server.command;
+package by.bsuir.anna.wt.tasks.third.server.command;
 
-import by.bsuir.dshparko.wt.tasks.third.server.command.exception.CommandException;
+import by.bsuir.anna.wt.tasks.third.server.command.exception.CommandException;
 
 public interface Command {
     String complete(Object caller, String request) throws CommandException;
