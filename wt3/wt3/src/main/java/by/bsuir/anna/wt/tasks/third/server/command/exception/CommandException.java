bad20475c0a9879f8fce4ea445bd16b05822a418
makarushko <71987147+makarushko@users.noreply.github.com>
2021-12-14 05:20:03 +0300

    Update CommandException.java

--- wt3/wt3/src/main/java/by/bsuir/anna/wt/tasks/third/server/command/exception/CommandException.java
+++ wt3/wt3/src/main/java/by/bsuir/anna/wt/tasks/third/server/command/exception/CommandException.java
@@ -1,4 +1,4 @@
-package by.bsuir.dshparko.wt.tasks.third.server.command.exception;
+package by.bsuir.anna.wt.tasks.third.server.command.exception;
 
 public class CommandException extends Exception{
     public CommandException(String message) {
